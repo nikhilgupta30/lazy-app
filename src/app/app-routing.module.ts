f81eb3d81a4aa939fa9569de7e4d26034bd8da3a
Nikhil Gupta <nikhilgupta311@gmail.com>
2021-06-10 13:58:29 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'lazy', loadChildren: () => import('src/app/lazy/lazy.module').then(m => m.LazyModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
